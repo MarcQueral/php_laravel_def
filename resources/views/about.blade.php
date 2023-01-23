1bf6052bf947a5be88d9d0a1309b3c00d1bd677d
MarcQueral <mqueral@iesebre.com>
2023-01-23 15:20:38 +0100

    WIP

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    <div class="py-10">
+        <header>
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <h1 class="text-3xl font-bold leading-tight tracking-tight text-gray-900">About</h1>
+            </div>
+        </header>
+        <main>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam commodi id nostrum qui sit, tempore tenetur voluptas! Dolorem excepturi facilis hic, ipsam libero maiores molestiae nobis non optio sed.
+        </main>
+    </div>
+</x-layout>
