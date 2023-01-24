f9529473290baba57690c143217c22424f5061ef
MarcQueral <mqueral@iesebre.com>
2023-01-24 11:36:57 +0100

    Video 11 acabat

--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,10 @@
 <?php
 
-use App\Models\Task;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-//    $task1 = Task::create([
-//        'title' => 'Tasca 1',
-//        'description' => 'lorem bla bla bla',
-//        'completed' => 0
-//    ]);
-
-//    $task2 = Task::create([
-//        'title' => 'Tasca 2',
-//        'description' => 'lorem bla bla bla',
-//        'completed' => 1
-//    ]);
-
-//    $task3 = Task::create([
-//        'title' => 'Tasca 3',
-//        'description' => 'lorem bla bla bla',
-//        'completed' => 1
-//    ]);
-
-//    $tasks = [
-//        $task1,
-//        $task2,
-//        $task3
-//    ];
-
-    return view('tasks',[
-        'tasks' => Task::all()
-    ]);
-});
-
-Route::get('/tasks', function () {
-    return view('tasks',[
-        'tasks' => []
-    ]);
-});
+Route::get('/', [TaskController::class,'index']);
+Route::get('/tasks', [TaskController::class,'index']);
 
 Route::get('/users', function () {
     return view('users');
